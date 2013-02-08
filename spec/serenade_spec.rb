449c8de2d1fa9cdd07446ee0eab93fd0acd1ce8b
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2013-02-08 18:04:36 +0100

    Tagged 0.4.1

--- spec/serenade_spec.rb
+++ spec/serenade_spec.rb
@@ -6,7 +6,7 @@ describe Serenade do
   describe Serenade::Renderer do
     describe "#parse" do
       it "returns a parsed Sereande template" do
-        result = Serenade::Renderer.new("foo", 'h1 "Hello world"').parse
+        result = Serenade::Renderer.new("foo", 'h1 "Hello world"').parse[0]
         result["name"].should eq "h1"
         result["children"][0]["type"].should eq "text"
         result["children"][0]["value"].should eq "Hello world"
